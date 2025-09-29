85169d1589e76551158c950800df6e7b968c4680
unknown <allanapaganini11@gmail.com>
2025-09-29 00:22:22 -0300

    projeto final update8

--- projeto final/cinematch-api/src/app.controller.ts
+++ projeto final/cinematch-api/src/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller()
+@Controller() // <-- decorator correto
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  redirectToMovies() {
+    return { message: "Acesse /movies para ver os filmes" };
   }
 }
